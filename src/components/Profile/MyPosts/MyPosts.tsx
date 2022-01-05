8867a9d45a7ebb5033a77008e8b3f3e816f94d64
AlexMaystrenko <maystrenko_alex@mail.ru>
2022-01-05 23:55:06 +0300

    sn l-17 used props

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -11,9 +11,9 @@ export const MyPosts = () => {
           <button>Add post</button>
         </div>
         <div className={s.posts}>          
-          <Post message='How are you'/>
-          <Post message='Hey'/>
-          <Post message='Yoo' />
+          <Post message='How are you' likeCount={1}/>
+          <Post message="It\'s my first post" likeCount={13}/>
+          <Post message='Yoo' likeCount={7}/>
         </div>
       </div>
   );
